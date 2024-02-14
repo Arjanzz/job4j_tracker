d57a1878e0b45dc91c5896a5824031fd80417696
Hotsko Vladimir <malletgamer@mail.ru>
2024-02-14 22:23:28 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,29 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, items.length);
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                count++;
+            }
+        }
+
+        Item[] result = new Item[count];
+        int index = 0;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(key)) {
+                result[index] = item;
+                index++;
+            }
+        }
+        return result;
     }
 
     public Item findById(int id) {
