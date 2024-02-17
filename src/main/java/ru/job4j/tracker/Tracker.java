0deff4199403708f3049025dfeb38e164d7c00c8
Hotsko Vladimir <malletgamer@mail.ru>
2024-02-17 08:36:11 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,19 +29,31 @@ public class Tracker {
         return Arrays.copyOf(result, index);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int result = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                result = index;
                 break;
             }
         }
-        return rsl;
+        return result;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public boolean replace(int id, Item item) {
-        return false;
+        int index = indexOf(id);
+        if (index != -1) {
+            int oldId = items[index].getId();
+            items[index] = item;
+            items[index].setId(oldId);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
