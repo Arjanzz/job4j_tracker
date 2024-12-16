115ed8927f4a991a5c976dee14d590b002dcff68
Hotsko Vladimir <malletgamer@mail.ru>
2024-12-16 10:36:08 +0300

    0.3. Тестирование исключений с junit [#219366 #525365]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -14,4 +14,14 @@ class FactorialTest {
                 () -> Factorial.calc(-1));
         assertThat(exception.getMessage()).isEqualTo("Number could not be less than 0");
     }
+
+    @Test
+    void whenNotException() {
+        assertThat(Factorial.calc(0)).isEqualTo(1);
+        assertThat(Factorial.calc(1)).isEqualTo(1);
+        assertThat(Factorial.calc(2)).isEqualTo(2);
+        assertThat(Factorial.calc(3)).isEqualTo(6);
+        assertThat(Factorial.calc(4)).isEqualTo(24);
+        assertThat(Factorial.calc(5)).isEqualTo(120);
+    }
 }
