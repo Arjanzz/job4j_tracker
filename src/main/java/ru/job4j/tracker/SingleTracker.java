2bd3f429e3e1b256af600eedf677514dece41b95
Hotsko Vladimir <malletgamer@mail.ru>
2024-12-24 10:14:38 +0300

    1. Singleton [#94606 #526340]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,7 +1,17 @@
 package ru.job4j.tracker;
 
 public class SingleTracker {
-    private Tracker tracker = new Tracker();
+    private static Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
