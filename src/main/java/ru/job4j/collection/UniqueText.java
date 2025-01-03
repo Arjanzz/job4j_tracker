82214b7dbe35d4a2d56ada3a47a9c37a375d8ed7
Hotsko Vladimir <malletgamer@mail.ru>
2025-01-03 16:58:10 +0300

    2. Идентичные тексты [#212655 #527095]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class UniqueText {
     public boolean isEquals(String originText, String duplicateText) {
-        boolean result = true;
+
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -17,6 +17,6 @@ public class UniqueText {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
