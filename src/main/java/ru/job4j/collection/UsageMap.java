19475e6e37770bfef7ef74421ac5a310d098ff32
Hotsko Vladimir <malletgamer@mail.ru>
2025-01-04 21:58:26 +0300

    0. Map, HashMap [#212652 #527216]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.put("parsentev@mail.ru", "Dmitry Sergeevich");
+        map.put("parsentev@rumbler.ru", "Sergey Sergeevich");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
